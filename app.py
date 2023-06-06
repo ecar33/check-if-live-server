30d142bb3603156f7e822365edcbf7cbd24356a3
Evan Carlile <114954921+ecar33@users.noreply.github.com>
2023-06-06 14:22:26 -0500

    Transferring

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-from flask import Flask, redirect, url_for, flash
+from flask import Flask, redirect, url_for, flash, render_template, request
 from flask import session
 from flask_dance.contrib.twitch import make_twitch_blueprint, twitch
 from dotenv import load_dotenv
 import os
-import logging
 import requests
+from oauthlib.oauth2.rfc6749.errors import MissingTokenError
+from create_logger import create
 
 load_dotenv()
 os.environ['FLASK_ENV'] = 'testing'
-#logging.basicConfig(level=logging.WARNING)
 
 
-# Create a logger
-logger = logging.getLogger('my_logger')
 
-# Set the level of this logger to DEBUG. This means it will handle messages of severity DEBUG and above.
-logger.setLevel(logging.DEBUG)
-
-# Create a file handler
-file_handler = logging.FileHandler('my_log_file.log')
-
-# Set the level of the file handler to DEBUG. This means it will handle messages of severity DEBUG and above.
-file_handler.setLevel(logging.DEBUG)
-
-# Create a formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# Add the formatter to the file handler
-file_handler.setFormatter(formatter)
-
-# Add the file handler to the logger
-logger.addHandler(file_handler)
+logger = create()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
@@ -42,59 +24,74 @@ twitch_blueprint = make_twitch_blueprint(
 )
 app.register_blueprint(twitch_blueprint)
 
+
 @app.route('/')
 def index():
-    if twitch.authorized:
-        return redirect(url_for("dashboard"))
-    else:
+    logger.debug("You are in index.")
+    try:
+        if twitch.authorized:
+            resp = twitch.get("user")  # make an API call
+            if resp.status_code == 200:
+                # API call was successful, so the token is valid
+                return redirect(url_for("dashboard"))
+    except MissingTokenError:
         return redirect(url_for("twitch.login"))
 
+    return redirect(url_for("twitch.login"))
 
-@app.route('/twitch/authorized')
-def twitch_callback():
-    logger.debug("You reached callback")
-    response = twitch.authorized_response()
-    if response is None or response.get('access_token') is None:
-        print("Response_failed")
-        logger.debug('Failed to log in: %s', response)
-        flash("Failed to log in.", category="error")
-        return redirect(url_for("index"))
-
-
-    logger.debug('Logged in successfully: %s', response)
-    # Save the user's logged-in state to their session here
-    session['logged_in'] = True
-    session['access_token'] = response.get('access_token')
-    return redirect(url_for("dashboard"))
 
 @app.route('/dashboard')
 def dashboard():
     if twitch.authorized:
-        headers = {
-            "Authorization": f"OAuth {twitch.access_token}"
-        }
-        response = requests.get("https://id.twitch.tv/oauth2/validate", headers=headers)
-        logger.debug(response.text)
-        if response.status_code == 200:
-            # Token is valid
-            headers = {
-                "Authorization": f"Bearer {twitch.access_token}",
-                "Client-ID": app.config['TWITCH_OAUTH_CLIENT_ID']
-            }
-            user_data_url = "https://api.twitch.tv/helix/users/"
-            resp = requests.get(user_data_url, headers=headers)
-            if resp.ok:
-                return resp.json()
-            else:
-                logger.debug(resp.status_code)
-                return "Something went wrong"
-        else:
-            # Token is invalid or expired
-            logger.debug(response.status_code)
-            return redirect(url_for("twitch.login"))
+        try:
+            username, email, user_id = get_user_info(twitch)
+            streamer_names = get_followed_streamers(twitch, user_id)
+
+            return render_template("dashboard.html", username=username, email=email,
+                                   streamer_names=streamer_names)
+        except Exception as e:
+            logger.debug(e)
+            return []
     else:
         return redirect(url_for("index"))
 
 
-if __name__ == '__main__':
-    app.run()
+def get_user_info(twitch):
+    headers = {
+        "Authorization": f"Bearer {twitch.access_token}",
+        "Client-ID": app.config['TWITCH_OAUTH_CLIENT_ID']
+    }
+    user_data_url = "https://api.twitch.tv/helix/users/"
+    resp = requests.get(user_data_url, headers=headers)
+    if resp.ok:
+        resp = resp.json()
+        display_name = resp["data"][0]["display_name"]
+        email = resp["data"][0]["email"]
+        user_id = resp["data"][0]["id"]
+        return display_name, email, user_id
+    else:
+        logger.debug(resp.status_code)
+        return []
+
+
+def get_followed_streamers(twitch, user_id):
+    headers = {
+        "Authorization": f"Bearer {twitch.access_token}",
+        "Client-ID": app.config['TWITCH_OAUTH_CLIENT_ID']
+    }
+
+    followed_streamers_url = f'https://api.twitch.tv/helix/channels/followed?user_id={user_id}'
+    resp = requests.get(followed_streamers_url, headers=headers)
+    if resp.ok:
+        resp = resp.json()
+        streamer_names = []
+        for item in resp['data']:
+            streamer_names.append(item['broadcaster_name'])
+        return streamer_names
+    else:
+        logger.debug(resp)
+        return []
+
+def get_cookies():
+    cookies = request.cookies
+    return f"Cookies: {cookies}"
